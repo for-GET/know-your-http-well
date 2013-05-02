a80201f2234f092a8d2b2ae4f9b5d135fb0c460a
Andrei Neculau <andrei.neculau@gmail.com>
2013-05-02 23:11:53 +0200

    update emacs packages

---
+++ emacs/know-your-http-well.el
@@ -0,0 +1,11 @@
+;;; know-your-http-well.el --- collection of HTTP packages
+;;; Commentary:
+;;; Code:
+
+(require 'http-headers)
+(require 'http-methods)
+(require 'http-relations)
+(require 'http-status-codes)
+
+(provide 'know-your-http-well)
+;;; know-your-http-well.el ends here
